2b99a3da5a8b53669626458a585a3e0b75b56539
FireLord <firelord.xda@gmail.com>
2023-07-13 12:03:44 +0530

    app: add basic column for as list

--- app/src/main/java/com/firelord/feed/MainActivity.kt
+++ app/src/main/java/com/firelord/feed/MainActivity.kt
@@ -3,44 +3,40 @@ package com.firelord.feed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.firelord.feed.ui.theme.FeedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FeedTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ScrollableColumnDemo()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ScrollableColumnDemo(){
+    Column {
+        for (i in 1..100){
+            Text(
+                "User Name $i",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(10.dp)
+            )
+            Divider(color = Color.Black, thickness = 5.dp)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FeedTheme {
-        Greeting("Android")
     }
 }
